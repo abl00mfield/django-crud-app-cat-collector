51d7d6bd090b977432f3d23549ff3b3a115cff97
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-23 12:20:37 -0600

    create, update, delete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 
 # create your views here
@@ -7,36 +8,43 @@ from django.http import HttpResponse
 # we call theses view funcitons
 def home(request):
     # each view receives a request object
-    return HttpResponse("<h1>Hello ᓚᘏᗢ</h1>")
-    # we return an HttpResponse object to respond
+    return render(request, "home.html")
+
+
+# we return an HttpResponse object to respond
 
 
 def about(request):
-    return render(request, "about.html")  # same thing as res.render
+    contact_details = "contact us at help@gmail.com"
+    return render(
+        request, "about.html", {"contact": contact_details}
+    )  # same thing as res.render
 
-
-# views.py
-
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-
-# Create a list of Cat instances
-cats = [
-    Cat("Lolo", "tabby", "Kinda rude.", 3),
-    Cat("Sachi", "tortoiseshell", "Looks like a turtle.", 0),
-    Cat("Fancy", "bombay", "Happy fluff ball.", 4),
-    Cat("Bonk", "selkirk rex", "Meows loudly.", 6),
-]
 
 # views.py
 
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, "cats/index.html", {"cats": cats})
+
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, "cats/detail.html", {"cat": cat})
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = ["name", "breed", "description", "age"]
+
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ["breed", "description", "age"]
+
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = "/cats/"
